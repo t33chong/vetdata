f24c525218aa3388c40df24a8df23ff5fd932156
Tristan Euan Chong <tristaneuan@gmail.com>
2013-09-04 21:47:49 +0000

    added id column

--- vetdata.py
+++ vetdata.py
@@ -1,9 +1,9 @@
 import os, subprocess, requests
 
-print "=url=".ljust(40)+"\t==expected==\t==actual=="
+print "=id=\t=url=".ljust(40)+"\t==expected==\t==actual=="
 for wid in os.listdir('/data/xml'):
     xmlCount = sum([len(os.listdir('/data/xml/%s/%s' % (wid, subdir))) for subdir in os.listdir('/data/xml/'+wid)])
-    doc = requests.get('http://search-s10:8983/solr/xwiki/select', params={'wt':'json', 'q':'id:%s'%wid, 'fl':'url,articles_i'}).json().get('response', {}).get('docs', [{}])[0]
-    print "%s\t%s\t%s" % (doc.get('url').ljust(40), str(doc.get('articles_i')).ljust(10), str(xmlCount).ljust(10))
+    doc = requests.get('http://search-s10:8983/solr/xwiki/select', params={'wt':'json', 'q':'id:%s'%wid, 'fl':'id,url,articles_i'}).json().get('response', {}).get('docs', [{}])[0]
+    print "%s\t%s\t%s\t%s" % (str(doc.get('id')), doc.get('url').ljust(40), str(doc.get('articles_i')).ljust(10), str(xmlCount).ljust(10))
 
         
