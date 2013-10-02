0fcd8bd883d6a4a7aa47a76fce7bb6c973c57000
Tristan Euan Chong <tristaneuan@gmail.com>
2013-10-02 23:47:38 +0000

    updated to remove solr dependency

--- id_missing.py
+++ id_missing.py
@@ -1,25 +1,29 @@
 import re
-from time import sleep
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
-from WikiaSolr import QueryIterator
 
 bucket = S3Connection().get_bucket('nlp-data')
 
-qi = QueryIterator('http://search-s10:8983/solr/xwiki/', {'query': 'lang_s:en', 'fields': 'id', 'sort': 'id asc'})
+print 'loading titles...'
+titles = dict((re.search('/([0-9]+)\.gz', title.key).group(1), True) for title in bucket.list(prefix='article_titles/') if re.search('/([0-9]+)\.gz', title.key))
+print 'loading redirects...'
+redirects = dict((re.search('/([0-9]+)\.gz', redirect.key).group(1), True) for redirect in bucket.list(prefix='article_redirects/') if re.search('/([0-9]+)\.gz', redirect.key))
 
 t = open('missing-titles.txt', 'w')
 r = open('missing-redirects.txt', 'w')
 
-for doc in qi:
-    wid = doc['id']
-    print 'checking', wid
-    if not bucket.get_key('article_titles/%s.gz' % wid):
-        print wid, 'missing from titles'
-        t.write('%s\n' % wid)
-    if not bucket.get_key('article_redirects/%s.gz' % wid):
-        print wid, 'missing from redirects'
-        r.write('%s\n' % wid)
+for parsed in bucket.list(prefix='xml/', delimiter='/'):
+    #print parsed.name
+    s = re.search('([0-9]+)', parsed.name)
+    if s:
+        print 'checking', s
+        wid = s.group(0)
+        if not titles.get(wid, False):
+            print wid, 'not found in titles!'
+            t.write('%s\n' % wid)
+        if not redirects.get(wid, False):
+            print wid, 'not found in redirects!'
+            r.write('%s\n' % wid)
 
 t.close()
 r.close()
